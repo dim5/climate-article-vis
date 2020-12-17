4dfc2b971b51eca7d1cf77d05d72375a9571c108
Daniel Marczin <dim5@users.noreply.github.com>
2020-12-18 00:06:04 +0100

    created top entities display

---
+++ src/components/top-entities.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components/macro';
+import Top15 from '../data/top15.json';
+
+const PercentItem = styled.div`
+  border-bottom: 4px solid black;
+  border-image: linear-gradient(
+    to right,
+    forestgreen ${(props) => props.percent}%,
+    rgba(0, 0, 0, 0) ${(props) => props.percent}%
+  );
+  border-image-slice: 1;
+`;
+
+const TopEntities = ({ className }) => {
+  return (
+    <div className={className}>
+      {Top15.map((e) => {
+        return (
+          <PercentItem percent={e.pct} key={e.ent}>
+            {e.ent}{' '}
+            <span
+              css={`
+                float: right;
+              `}
+            >
+              {e.cnt}
+            </span>
+          </PercentItem>
+        );
+      })}
+    </div>
+  );
+};
+
+export default TopEntities;
